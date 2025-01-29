ebe2cdb486cf862a99b6a8ef2a7cfbd8aefe000e
SayarPatel <sayarjpatel001@gmail.com>
2025-01-29 23:13:46 +0530

    gym website template

---
+++ Task/Projects/React js/development/src/components/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div id='contact'>
+      <h1>CONTACT US</h1>
+      <form action="">
+        <input type="text" name="" id="" placeholder='Full Name'/>
+        <input type="email" name="" id="" placeholder='Type Your E-mail'/>
+        <textarea name="" id="" placeholder='Write Here......'></textarea>
+        <input type="submit" value='Send' />
+      </form>
+    </div>
+  )
+}
+
+export default Contact
